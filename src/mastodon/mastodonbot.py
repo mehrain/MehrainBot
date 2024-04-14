9939ab214769ec0a03f7b5b54fb3c51160df9dd7
mehrain <marijnrovers@live.nl>
2024-04-14 02:06:38 +0200

    Now posting to Mastadon. Will extend features further. Tomorrow ill implement author checks to prevent trolls

---
+++ src/mastodon/mastodonbot.py
@@ -0,0 +1,25 @@
+from mastodon import Mastodon
+import os
+from mastodon import Mastodon
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class MastodonBot:
+    def __init__(self):
+        self.mastodon = Mastodon(
+            access_token=os.getenv('MASTODON_ACCESS_TOKEN'),
+            api_base_url=os.getenv('MASTODON_API_BASE_URL')
+        )
+
+    def post_status(self, status):
+        self.mastodon.status_post(status)
+
+    def get_timeline(self):
+        return self.mastodon.timeline_home()
+
+    def get_mentions(self):
+        return self.mastodon.notifications()
+    
+
+
