a60fccf57e7fdf8823e47ee67844cb0ca6feed58
mehrain <marijnrovers@live.nl>
2024-04-15 22:25:16 +0200

    added authentication for posting to prevent trolls

--- src/commands/socials.py
+++ src/commands/socials.py
@@ -8,6 +8,7 @@ from discord import guild_only
 from src.socials.mastodonbot import MastodonBot
 
 GUILD_IDS = [int(os.getenv('MCP_GUILD_ID'))]
+MEHRAIN_ID = int(os.getenv('MEHRAIN_ID'))
 
 class Socials(commands.Cog):
     def __init__(self, bot):
@@ -18,11 +19,14 @@ class Socials(commands.Cog):
     @guild_only()
     @option("post_text", description="Enter your post text", required=True)
     async def mastodon(self, ctx : discord.ApplicationContext, post_text:str):
-        """Mastodon Function Calls"""  # The command description can be supplied as the docstring
-             
-        self.mastodonBot.post_status(post_text) 
-        post_text = str(post_text)
-        await ctx.respond(f"The following message has been send to Mastodon: {post_text}")
+        """Mastodon Function Calls"""  
+        if ctx.author.id == MEHRAIN_ID:     
+            self.mastodonBot.post_status(post_text) 
+            post_text = str(post_text)
+            await ctx.respond(f"The following message has been send to Mastodon: {post_text}")
+        else:
+            author = str(ctx.author.display_name)
+            await ctx.respond(f"Stop trolling {author}! You are not allowed to use this command.")
 
 def setup(bot): # this is called by Pycord to setup the cog
     bot.add_cog(Socials(bot)) # add the cog to the bot
