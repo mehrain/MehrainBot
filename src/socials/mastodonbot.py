c4edc5c1ae66222548037879c30bf4224d89f624
mehrain <marijnrovers@live.nl>
2024-04-16 23:51:38 +0200

    need a good botserver first.

--- src/socials/mastodonbot.py
+++ src/socials/mastodonbot.py
@@ -1,23 +1,14 @@
 from mastodon import Mastodon, StreamListener
 import os
 
-class MyStreamListener(StreamListener):
-    def __init__(self, mastodon):
-        self.mastodon = mastodon
-
-    def on_update(self, status):
-        if '@your_bot_username' in status.content:
-            self.mastodon.status_post('@' + status.account.username + ' Hello there, i am a bot! If you have feedback or questions, please contact my creator.')
-
 class MastodonBot:
     def __init__(self):
         self.mastodon = Mastodon(
             access_token=os.getenv('MASTODON_ACCESS_TOKEN'),
             api_base_url=os.getenv('MASTODON_API_BASE_URL')
         )
-        self.listener = MyStreamListener(self.mastodon)
-        self.start_streaming()
-
+        #self.start_streaming()
+        
     def post_status(self, status):
         self.mastodon.status_post(status)
 
@@ -27,9 +18,17 @@ class MastodonBot:
     def get_mentions(self):
         return self.mastodon.notifications()
 
+    def handle_mention(self, status):
+        if '@your_bot_username' in status.content:
+            self.mastodon.status_post('@' + status.account.username + ' Hello there, i am a bot! If you have feedback or questions, please contact my creator.')
+
+
     def start_streaming(self):
         try:
-            self.mastodon.stream_user(self.listener)
+            self.mastodon.stream_user(self.handle_mention)
             print("Mastodon streaming started")
         except Exception as e:
             print(f"An error occurred while streaming: {e}")
+    
+
+
