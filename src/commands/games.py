c6aa41e3955dcb660d34d2c1619d70180d74f586
Mehrain <marijnrovers@live.nl>
2024-04-21 22:44:48 +0200

    First pokemon logic (#4)
    Co-authored-by: Tim <TimOomis@users.noreply.github.com>

--- src/commands/games.py
+++ src/commands/games.py
@@ -7,6 +7,7 @@ from discord import guild_only
 
 from src.games import rps
 from src.games import coinflip
+from src.games.pokeapi import PokeApi
 
 GUILD_IDS = [int(id) for id in os.getenv('GUILD_IDS').split(',')]
 
@@ -14,6 +15,7 @@ GUILD_IDS = [int(id) for id in os.getenv('GUILD_IDS').split(',')]
 class Games(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.pokeApi = PokeApi()
 
     @discord.slash_command(name="rps", guild_ids=GUILD_IDS)
     @guild_only()
@@ -36,6 +38,46 @@ class Games(commands.Cog):
         """Flip a coin and get heads or tails!"""
         result = coinflip.coin_flip(choice)
         await ctx.respond(result)
+        
+    @discord.slash_command(name="pokemon", guild_ids=GUILD_IDS)
+    @guild_only()
+    @option("pokemon_name", description="Enter your Pokemon name", required=True)
+    async def poke_search(self, ctx: discord.ApplicationContext, pokemon_name:str):
+        """Search for a Pokemon!"""
+        pokemon = self.pokeApi.get_pokemon(pokemon_name, is_shiny=True, is_female=False)  # replace with your function to search for a Pokemon
+
+        if pokemon is None:
+            await ctx.respond(f"No Pokemon found with the name {pokemon_name}")
+        else:
+            embed = discord.Embed(
+                title=pokemon.name,
+                description="Placeholder more stats will be added soon!",
+                color=discord.Colour.blurple(), # Pycord provides a class with default colors you can choose from
+            )
+
+            embed.set_image(url=pokemon.sprite)
+
+            await ctx.respond(" ", embed=embed)
+            
+            #async def hello(ctx):
+                
+    # embed = discord.Embed(
+    #     title="My Amazing Embed",
+    #     description="Embeds are super easy, barely an inconvenience.",
+    #     color=discord.Colour.blurple(), # Pycord provides a class with default colors you can choose from
+    # )
+    # embed.add_field(name="A Normal Field", value="A really nice field with some information. **The description as well as the fields support markdown!**")
+
+    # embed.add_field(name="Inline Field 1", value="Inline Field 1", inline=True)
+    # embed.add_field(name="Inline Field 2", value="Inline Field 2", inline=True)
+    # embed.add_field(name="Inline Field 3", value="Inline Field 3", inline=True)
+ 
+    # embed.set_footer(text="Footer! No markdown here.") # footers can have icons too
+    # embed.set_author(name="Pycord Team", icon_url="https://example.com/link-to-my-image.png")
+    # embed.set_thumbnail(url="https://example.com/link-to-my-thumbnail.png")
+    # embed.set_image(url="https://example.com/link-to-my-banner.png")
+ 
+    # await ctx.respond("Hello! Here's a cool embed.", embed=embed) # Send the embed with some text
 
 def setup(bot): # this is called by Pycord to setup the cog
     bot.add_cog(Games(bot)) # add the cog to the bot
---
+++ src/games/pokeapi.py
@@ -0,0 +1,44 @@
+import pokebase as pb
+
+class PokemonStat:     
+    def __init__(self, name, base_stat, effort):
+        self.name = name
+        self.base_stat = base_stat
+        self.effort = effort
+        self.url = f'https://pokeapi.co/api/v2/stat/{name}/'
+
+    def __str__(self):
+        return f'{self.name} - {self.base_stat} - {self.effort}'
+    
+    def __repr__(self):
+        return f'{self.name} - {self.base_stat} - {self.effort}'
+
+
+class PokeApi:
+        
+    def get_pokemon(self, poke_name, is_shiny, is_female):
+        pokemon = pb.pokemon(poke_name)
+        sprite = pb.SpriteResource('pokemon', pokemon.id, female=is_female, back=False, other=False, shiny=is_shiny)
+        
+        stats = []
+        
+        for stat in pokemon.stats:
+            stats.append(PokemonStat(stat.stat.name, stat.base_stat, stat.effort))
+
+        # Create object to return.
+        return PokeResponse(pokemon.name, sprite.url, stats) 
+
+class PokeResponse:
+    
+    def __init__(self, name, sprite, stats):
+        self.name = name
+        self.sprite = sprite
+        self.stats = stats
+    
+# api = PokeApi()
+# pokemon = api.get_pokemon('pikachu', False, False)
+
+# print(pokemon)
+# print(pokemon.name)
+# print(pokemon.sprite)
+# print(pokemon.stats)
