52b59a301242b92f5ede676de67f4f6f69979d51
mehrain <marijnrovers@live.nl>
2024-04-21 17:35:05 +0200

    discord notification upon getting followed implemented

--- src/socials/mastodonbot.py
+++ src/socials/mastodonbot.py
@@ -14,12 +14,14 @@ class MyStreamListener(StreamListener):
             self.mastodon.handle_follow(notification)
 
 class MastodonBot:
-    def __init__(self):
+    def __init__(self, discord_bot, discord_channels):
         self.mastodon = Mastodon(
             access_token=os.getenv('MASTODON_ACCESS_TOKEN'),
             api_base_url=os.getenv('MASTODON_API_BASE_URL')
         )
         self.listener = MyStreamListener(self)
+        self.discord_bot = discord_bot
+        self.discord_channels = discord_channels
         self.start_streaming()
 
     def handle_mention(self, status):
@@ -34,9 +36,14 @@ class MastodonBot:
 
     def handle_follow(self, notification):
         follower_id = notification.account.id
+        username = notification.account.acct
         self.mastodon.account_follow(follower_id)
-        #self.post_status(notification.account.acct, 'Thanks for following my bot!')
-        print(f"Followed {follower_id}")
+        print(f"Followed {username}")
+        for channel in self.discord_channels:
+            if channel:
+                self.discord_bot.loop.create_task(channel.send(f"```{username} followed MehrainBot, bot has followed them back```"))
+            else: 
+                print("Channel list not found, did you set your .env file correctly?")
 
     def post_status(self, status):
         self.mastodon.status_post(status)
